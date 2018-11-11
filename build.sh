4d2d15b3b42f81d22f887132fbab74e3b5d70ce3
thomas <thomas.steffens@outlook.com>
2018-11-11 15:16:00 +0100

    node

--- build.sh
+++ build.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
-cd ./*wasm
+
+RUST_LIB_PATH="./rust_lib"
+RUST_LIB_NAME="rust_lib"
+BINDGEN_OUTPUT_FOLDER_PATH="./wasm_bindgen_output"
+NODE_FOLDER_PATH="./node"
+WWW_ROOT_PATH="./www_root"
+WWW_ROOT_SCRIPT_PATH="$WWW_ROOT_PATH/script"
+
+if [ ! -d "$BINDGEN_OUTPUT_FOLDER_PATH" ];
+  then
+    echo "Creating bindgen output folder"
+    mkdir $BINDGEN_OUTPUT_FOLDER_PATH
+fi
+
+cd $RUST_LIB_PATH
 cargo build --release --target wasm32-unknown-unknown
 cd ..
-wasm-bindgen ./wasm/target/wasm32-unknown-unknown/release/wasm.wasm --out-dir ./wasm-bindgen-output
+
+wasm-bindgen $RUST_LIB_PATH/target/wasm32-unknown-unknown/release/$RUST_LIB_NAME.wasm --out-dir $BINDGEN_OUTPUT_FOLDER_PATH
 echo __Bindgen: Succes__
-cp ./wasm-bindgen-output/*.wasm ./node/src/
-cp ./wasm-bindgen-output/*.js ./node/src/
+
+cp $BINDGEN_OUTPUT_FOLDER_PATH/*.wasm $NODE_FOLDER_PATH/src/
+cp $BINDGEN_OUTPUT_FOLDER_PATH/*.js $NODE_FOLDER_PATH/src/
 echo __Moved WASM and JS to node/src__
-cd ./node
+
+cd $NODE_FOLDER_PATH
 #npm install
-npx webpack
-rm -R ../../http/dist
-mv ./dist ../../http
+npm run serve
+cd ..
