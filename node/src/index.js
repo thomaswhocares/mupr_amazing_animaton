4c36cee8e6d8474024f11bc90ac97677da5ceb70
thomas <thomas.steffens@outlook.com>
2018-11-21 15:48:53 +0100

    Restart

--- node/src/index.js
+++
@@ -1,8 +0,0 @@
-// Note that a dynamic `import` statement here is required due to
-// webpack/webpack#6615, but in theory `import { greet } from './hello_world';`
-// will work here one day as well!
-const rust = import('./rust_lib');
-
-rust
-  .then(m => m.greet('World!'))
-  .catch(console.error);
